0a4e50f63aba96b157ee50d9f349b253c63ccabf
crowmurk <crowmurk@gmail.com>
2019-04-24 15:36:15 +0300

    Create project settings

---
+++ locker/locker/settings/production.py
@@ -0,0 +1,98 @@
+from .base import *
+
+from ..log_filters import ManagementFilter
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.01',
+]
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/lib/miningstatistic/db.sqlite3',
+    }
+}
+
+# Logging
+
+verbose = (
+    "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(funcName)s [%(message)s]"
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'remove_migration_sql': {
+            '()': ManagementFilter,
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': verbose,
+            'datefmt': "%Y-%b-%d %H:%M:%S",
+        },
+    },
+    'handlers': {
+        'file': {
+            'filters': [
+                'remove_migration_sql',
+            ],
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/httpd/django.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': [
+                'file',
+            ],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+# Email
+
+# # Сообщения сохраняются в файл
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = '/var/log/httpd/django.mail'
+# Поле 'From' если не указано
+DEFAULT_FROM_EMAIL = 'no-reply@locker.local'
+# Поле 'From' если отправлено ADMINS и MANAGERS
+SERVER_EMAIL = 'contact@locker.local'
+# Префикс темы сообщения
+EMAIL_SUBJECT_PREFIX = '[Locker Calculator] '
+# Поле 'To' если отправлено MANAGERS
+MANAGERS = (('Us', 'ourselves@locker.local'),)
+# Поле 'To' если отправлено ADMINS
+ADMINS = (('Admin', 'admin@locker.local'),)
+
+# # Сообщения отправляются по smtp
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# # Поле 'From' если не указано
+# DEFAULT_FROM_EMAIL = 'my_support_mail@gmail.com'
+# # Поле 'From' если отправлено ADMINS и MANAGERS
+# SERVER_EMAIL = 'my_support_mail@gmail.com'
+# # Префикс темы сообщения
+# EMAIL_SUBJECT_PREFIX = '[Mining Statistic] '
+# # Поле 'To' если отправлено MANAGERS
+# MANAGERS = (('Us', 'my_secret_mail@gmail.com'),)
+# # Поле 'To' если отправлено ADMINS
+# ADMINS = (('Admin', 'my_secret_mail@gmail.com'),)
+#
+# # gmail
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'my_support_mail@gmail.com'
+# EMAIL_HOST_PASSWORD = 'my_secret_password'
