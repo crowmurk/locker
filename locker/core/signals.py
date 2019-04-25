196affd09587d120776010638fe3fc67cf38765b
crowmurk <crowmurk@gmail.com>
2019-04-25 11:09:15 +0300

    Add core app

---
+++ locker/core/signals.py
@@ -0,0 +1,40 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from core.utils import get_unique_slug
+
+
+@receiver(pre_save)
+def slug_create(sender, instance, *args, **kwargs):
+    """Создает slug при сохранении объекта в БД
+    """
+
+    # Аргументы вызова функции создания slug
+    args = {
+        'client': {
+            'Client': ('slug', 'name', True),
+        },
+    }
+
+    model_name = sender.__name__
+    instance_app = instance._meta.app_label
+
+    try:
+        # Извлекаем аргументы для модели
+        slug_field, *slugable_fields, unique = args[instance_app][model_name]
+
+        # Создаем slug только если он еще не задан
+        if getattr(instance, slug_field):
+            return None
+    except KeyError:
+        return None
+    else:
+        # Создаем slug
+        slug = get_unique_slug(
+            instance,
+            slug_field,
+            *slugable_fields,
+            unique=unique
+        )
+
+        setattr(instance, slug_field, slug)
