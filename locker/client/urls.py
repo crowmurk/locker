01ba664d3ebb03c4da4b67995cf3d054101ef079
crowmurk <crowmurk@gmail.com>
2019-04-26 17:15:46 +0300

    fix: Client urls regex

--- locker/client/urls.py
+++ locker/client/urls.py
@@ -8,7 +8,7 @@ app_name = 'client'
 urlpatterns = [
     path('', views.ClientList.as_view(), name='list'),
     path('create/', views.ClientCreate.as_view(), name='create'),
-    re_path(r'^client/(?P<slug>[\w-]+)/$', views.ClientDetail.as_view(), name='detail'),
-    re_path(r'^client/(?P<slug>[\w-]+)/update/$', views.ClientUpdate.as_view(), name='update'),
-    re_path(r'^client/(?P<slug>[\w-]+)/delete/$', views.ClientDelete.as_view(), name='delete'),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.ClientDetail.as_view(), name='detail'),
+    re_path(r'^(?P<slug>[\w-]+)/update/$', views.ClientUpdate.as_view(), name='update'),
+    re_path(r'^(?P<slug>[\w-]+)/delete/$', views.ClientDelete.as_view(), name='delete'),
 ]
