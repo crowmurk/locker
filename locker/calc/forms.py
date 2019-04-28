942eb0c142baa7ae29268c46f80a6c7f38563661
crowmurk <crowmurk@gmail.com>
2019-04-28 21:09:35 +0300

    feat: Add button Create client in form select widget

--- locker/calc/forms.py
+++ locker/calc/forms.py
@@ -1,10 +1,40 @@
 from django import forms
 from django.contrib.auth import get_user
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.forms import widgets
+from django.utils.translation import gettext_lazy as _
 
+from client.models import Client
 from .models import Order, Service, OrderOption
 
 
+class RelatedFieldWidgetCanAdd(widgets.Select):
+    """Append add button to Select widget
+    """
+    def __init__(self, related_model, related_url=None, *args, **kwargs):
+        super(RelatedFieldWidgetCanAdd, self).__init__(*args, **kwargs)
+        self.related_url = related_url
+
+    def render(self, name, value, *args, **kwargs):
+        self.related_url = reverse(self.related_url)
+        output = [super(RelatedFieldWidgetCanAdd, self).render(name, value, *args, **kwargs)]
+        output.append(
+            '&nbsp;<a href="{url}" class="button">{name}</a>'.format(
+                url=self.related_url,
+                name=_("Create"),
+            ),
+        )
+        return mark_safe(' '.join(output))
+
+
 class OrderForm(forms.ModelForm):
+    client = forms.ModelChoiceField(
+        required=True,
+        queryset=Client.objects.all(),
+        widget=RelatedFieldWidgetCanAdd(Client, related_url="client:create"),
+    )
+
     class Meta:
         model = Order
         exclude = ('author', )
