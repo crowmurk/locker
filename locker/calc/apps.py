5e4b3e201c9a93fd77dc65ecf7847cde69dd3e54
crowmurk <crowmurk@gmail.com>
2019-04-28 10:22:44 +0300

    fix: Add apps verbose names

--- locker/calc/apps.py
+++ locker/calc/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class CalcConfig(AppConfig):
     name = 'calc'
+    verbose_name = _('Calculator')
--- locker/client/apps.py
+++ locker/client/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ClientConfig(AppConfig):
     name = 'client'
+    verbose_name = _('Clients')
 
     def ready(self):
         import core.signals
-
