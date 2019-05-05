005d8cb01662b60500354694a53414366de41d00
crowmurk <crowmurk@gmail.com>
2019-05-05 08:30:18 +0300

    feat: Add post method to delete selected table rows

--- locker/core/views.py
+++ locker/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+class DeleteColumnMixin:
+    related_model = None
+    delete_button = "delete-table-items-button"
+
+    def post(self, request, *args, **kwargs):
+        button = request.POST.get(self.delete_button)
+        pks = request.POST.getlist(button)
+        if pks:
+            model = self.related_model if self.related_model else self.model
+            selected_objects = model.objects.filter(pk__in=pks)
+            selected_objects.delete()
+        return HttpResponseRedirect(request.path)
