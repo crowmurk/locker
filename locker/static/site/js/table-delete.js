fbe45bbb53d31c193ba68f6f714f3f5a4f3e988b
crowmurk <crowmurk@gmail.com>
2019-05-05 07:32:15 +0300

    refractor: function togle_rows

--- locker/static/site/js/table-delete.js
+++ locker/static/site/js/table-delete.js
@@ -1,26 +1,42 @@
-function toggle_rows( source,
-    { selectionName='delete-table-items',
-        selectionType='checkbox',
-        buttonName='delete-table-items-button',
-        buttonType='submit'}) {
+function toggle_rows(source=undefined, options={}) {
+    /* source = undefined - no header selector
+     *
+     * options = {} - default options:
+     *      options.selectionType = 'checkbox' - row selector type
+     *      options.selectionName = 'delete-table-items' - row selectors names
+     *      options.buttonType = 'submit' - delete button type
+     *      options.buttonName = 'delete-table-items-button' - delete button name
+     *
+     * options.selectionName  or selectionType = undefined - no rows checkboxes
+     * options.buttonName or buttonType = undefined - no button to toggle */
 
-    var checkboxes = $(["input[type='", selectionType, "'][name='", selectionName, "']"].join('')),
-        button = $(["button[type='", buttonType, "'][name='", buttonName, "']"].join(''));
+    if (arguments.length != 2) {return}
 
-    for(var i in checkboxes)
-        checkboxes[i].checked = source.checked;
+    // Default variables
+    var selectionName = 'selectionName' in options ? options.selectionName: 'delete-table-items',
+        selectionType = 'selectionType' in options ? options.selectionType: 'checkbox',
+        buttonName = 'buttonName' in options ? options.buttonName: 'delete-table-items-button',
+        buttonType = 'buttonType' in options ? options.buttonType: 'submit';
 
-    button.attr("disabled", !source.checked);
-};
+    // Rows checkboxes to toggle if not undefined explicitly
+    if (selectionName !== undefined && selectionType !== undefined)
+        var checkboxes = $(["input[type='", selectionType, "'][name='", selectionName, "']"].join(''));
 
-function toggle_row({
-    selectionName='delete-table-items',
-    selectionType='checkbox',
-    buttonName='delete-table-items-button',
-    buttonType='submit', }) {
+    // Delete button to toggle if not undefined explicitly
+    if (buttonName !== undefined && buttonType !== undefined)
+        var button = $(["button[type='", buttonType, "'][name='", buttonName, "']"].join(''));
 
-    var checkboxes = $(["input[type='", selectionType, "'][name='", selectionName, "']"].join('')),
-        button = $(["button[type='", buttonType, "'][name='", buttonName, "']"].join(''));
 
-    button.attr("disabled", !checkboxes.is(":checked"));
+    // Toggle row checkboxes
+    if (source !== undefined && checkboxes !== undefined)
+        for(var i in checkboxes)
+            checkboxes[i].checked = source.checked;
+
+    // Toggle delete button
+    if (button !== undefined) {
+        if (source !== undefined)
+            button.attr("disabled", !source.checked);
+        if (checkboxes !== undefined)
+            button.attr("disabled", !checkboxes.is(":checked"));
+    }
 };
