cd520baa7fab44fecae5c026e9ce9378a52a488e
crowmurk <crowmurk@gmail.com>
2019-05-11 15:04:32 +0300

    refractor: Filters for all tables

--- locker/calc/filters.py
+++ locker/calc/filters.py
@@ -1,27 +1,66 @@
-import django_filters
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+
+from django_filters import (
+    FilterSet,
+    CharFilter,
+    DateFromToRangeFilter,
+    NumberFilter,
+    RangeFilter,
+)
+from django_filters.widgets import RangeWidget
 
 from .models import Order, Service, OrderOption
 
 
-class OrderFilter(django_filters.FilterSet):
+class OrderFilter(FilterSet):
+    author = CharFilter(method='author_filter')
+    client = CharFilter(method='client_filter')
+    created = DateFromToRangeFilter(
+        widget=RangeWidget(
+            attrs={'placeholder': _('DD.MM.YYYY')},
+        ),
+    )
+
     class Meta:
         model = Order
-        fields = {
-            'author__last_name': ['icontains', ],
-        }
+        fields = []
+
+    def author_filter(self, queryset, name, value):
+        for word in value.split():
+            queryset = queryset.filter(
+                Q(author__first_name__icontains=word) | Q(author__last_name__icontains=word)
+            )
+        return queryset
+
+    def client_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(client__name__icontains=value) | Q(branch__name__icontains=value) | Q(branch__address__icontains=value)
+        )
 
 
-class ServiceFilter(django_filters.FilterSet):
+class ServiceFilter(FilterSet):
+    equipment = CharFilter(method='equipment_filter')
+    rating = RangeFilter()
+
     class Meta:
         model = Service
-        fields = {
-            'equipment': ['icontains', ],
-        }
+        fields = []
+
+    def equipment_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(equipment__icontains=value) | Q(work__icontains=value)
+        )
 
 
-class OrderOptionFilter(django_filters.FilterSet):
+class OrderOptionFilter(FilterSet):
+    order = NumberFilter()
+    equipment = CharFilter(
+        label=_('Service'),
+        field_name='service__equipment',
+        lookup_expr='icontains',
+    )
+
     class Meta:
         model = OrderOption
-        fields = {
-            'service__equipment': ['icontains', ],
-        }
+        fields = []
--- locker/client/filters.py
+++ locker/client/filters.py
@@ -1,19 +1,74 @@
-import django_filters
+from django.db.models import Count
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+
+from django_filters import (
+    FilterSet,
+    CharFilter,
+    RangeFilter,
+)
 
 from .models import Client, Branch
 
 
-class ClientFilter(django_filters.FilterSet):
+class ClientFilter(FilterSet):
+    client = CharFilter(
+        label=_('Client'),
+        method='client_filter',
+    )
+
+    orders = RangeFilter(
+        label=_('Orders'),
+        method='number_of_orders_filter',
+    )
+
     class Meta:
         model = Client
-        fields = {
-            'name': ['icontains', ],
-        }
+        fields = []
+
+    def client_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(details__icontains=value)
+        )
+
+    def number_of_orders_filter(self, queryset, name, value):
+        if value:
+            if value.start is not None and value.stop is not None:
+                lookup_expr = 'range'
+                value = (value.start, value.stop)
+            elif value.start is not None:
+                lookup_expr = 'gte'
+                value = value.start
+            elif value.stop is not None:
+                lookup_expr = 'lte'
+                value = value.stop
+
+            annotate_field = 'orders_number'
+            queryset = Client.objects.annotate(
+                **{annotate_field: Count('orders')},
+            )
+            queryset = queryset.filter(
+                **{'__'.join([annotate_field, lookup_expr]): value},
+            )
+
+        return queryset
 
 
-class BranchFilter(django_filters.FilterSet):
+class BranchFilter(FilterSet):
+    client = CharFilter(
+        label=_('Client'),
+        field_name='client__name',
+        lookup_expr='icontains',
+    )
+    branch = CharFilter(
+        label=_('Branch'),
+        method='branch_filter')
+
     class Meta:
         model = Branch
-        fields = {
-            'name': ['icontains', ],
-        }
+        fields = []
+
+    def branch_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(address__icontains=value)
+        )
