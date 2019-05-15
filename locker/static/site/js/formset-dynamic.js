497e15bb167cfcc83d41e12185b8f7dc695e6c07
crowmurk <crowmurk@gmail.com>
2019-05-15 07:13:51 +0300

    style: Add coments

--- locker/static/site/js/formset-dynamic.js
+++ locker/static/site/js/formset-dynamic.js
@@ -1,4 +1,5 @@
 function add_form(prefix, form, formset) {
+    // Add new form in formset
     var form_idx = $('#id_' + prefix + '-TOTAL_FORMS').val();
     $('#' + formset).append($('#' + form).html().replace(/__prefix__/g, form_idx));
     $('#id_' + prefix + '-TOTAL_FORMS').val(parseInt(form_idx) + 1);
