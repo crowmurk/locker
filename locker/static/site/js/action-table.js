9a593270d57603d530b517d9793eab1aa61d9d7d
crowmurk <crowmurk@gmail.com>
2019-05-20 19:38:33 +0300

    feat: Add DataTables to action_table

--- locker/static/site/js/action-table.js
+++ locker/static/site/js/action-table.js
@@ -40,3 +40,53 @@ function select_rows(source=undefined, options={}) {
             button.attr("disabled", !checkboxes.is(":checked"));
     }
 };
+
+$(document).ready(function() {
+    // Make table DataTable
+    var table = $('.table-container table')
+
+    var datatable_options = {
+        "filter": false,
+        "retrieve": true,
+        "columnDefs": [
+            { orderable: false, targets: -1 }
+        ]
+    }
+
+    // Russian language support
+    if ($("select[name='language']").val() == 'ru'){
+        datatable_options["language"] = {
+            "processing": "Подождите...",
+            "search": "Поиск:",
+            "lengthMenu": "Показать _MENU_ записей",
+            "info": "Записи с _START_ до _END_ из _TOTAL_ записей",
+            "infoEmpty": "Записи с 0 до 0 из 0 записей",
+            "infoFiltered": "(отфильтровано из _MAX_ записей)",
+            "infoPostFix": "",
+            "loadingRecords": "Загрузка записей...",
+            "zeroRecords": "Записи отсутствуют.",
+            "emptyTable": "В таблице отсутствуют данные",
+            "paginate": {
+                "first": "Первая",
+                "previous": "Предыдущая",
+                "next": "Следующая",
+                "last": "Последняя"
+            },
+            "aria": {
+                "sortAscending": ": активировать для сортировки столбца по возрастанию",
+                "sortDescending": ": активировать для сортировки столбца по убыванию"
+            }
+        }
+    }
+
+    // Remove hypelinks from table header
+    table.find('th.orderable a').contents().unwrap()
+
+    // Set DataTable paginator buttons classes
+    $.fn.dataTable.ext.classes.sPageButton = 'button';
+    $.fn.dataTable.ext.classes.sPageButtonActive = 'button-primary';
+
+    // Make DataTable
+    table.DataTable(datatable_options);
+    table.addClass("hover")
+});
