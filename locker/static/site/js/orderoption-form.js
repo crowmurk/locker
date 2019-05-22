8ba2b7a2d31504023f70ace4604f9c09129e24b1
crowmurk <crowmurk@gmail.com>
2019-05-22 07:02:24 +0300

    refractor: Remove variables declarations

--- locker/static/site/js/orderoption-form.js
+++ locker/static/site/js/orderoption-form.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
     // Add search option to select fields
-    var order_select = $("select[name='order'][id='id_order']");
-    var service_select = $("select[name='service'][id='id_service']");
-
-    order_select.select2();
-    service_select.select2();
+    $("select[name='order'][id='id_order']").select2();
+    $("select[name='service'][id='id_service']").select2();
 });
