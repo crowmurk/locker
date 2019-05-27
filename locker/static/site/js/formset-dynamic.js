2e978a977912075c3b1f0a851a38adbd0834e1a0
crowmurk <crowmurk@gmail.com>
2019-05-27 17:35:12 +0300

    refractor: Add form in formset

--- locker/static/site/js/formset-dynamic.js
+++ locker/static/site/js/formset-dynamic.js
@@ -1,6 +1,12 @@
-function add_form(prefix, form, formset) {
-    // Add new form in formset
-    var form_idx = $(`#id_${prefix}-TOTAL_FORMS`).val();
-    $(`#${formset}`).append($(`#${form}`).html().replace(/__prefix__/g, form_idx));
-    $(`#id_${prefix}-TOTAL_FORMS`).val(parseInt(form_idx) + 1);
-};
+$(document).ready(function() {
+    $('#formset_add_form_button').click(function() {
+        var options = $(this).data();
+
+        // Add new form in formset
+        if (options !== undefined) {
+            var form_idx = $(`#id_${options.prefix}-TOTAL_FORMS`).val();
+            $(`#${options.formset}`).append($(`#${options.form}`).html().replace(/__prefix__/g, form_idx));
+            $(`#id_${options.prefix}-TOTAL_FORMS`).val(parseInt(form_idx) + 1);
+        }
+    });
+});
