69dd7f29f8a64f575a381c23517a2b4f47faf509
crowmurk <crowmurk@gmail.com>
2019-05-30 14:42:58 +0300

    refractor: ActionTableDeleteMixin options

--- locker/core/views.py
+++ locker/core/views.py
@@ -7,32 +7,49 @@ from django.http import HttpResponseRedirect
 class ActionTableDeleteMixin:
     action_table_model = None
     action_table_button = "action-table-button"
-    action_table_multitables = dict()
-    success_delete_message = None
+    action_table_success_message = None
+    action_table_multitables = None
 
     def post(self, request, *args, **kwargs):
-        actions = dict()
         if self.action_table_model:
-            actions.update({self.action_table_button: self.action_table_model, })
-        actions.update(self.action_table_multitables)
-
-        if not actions:
+            self.action_table_multitables = [
+                {
+                    'model': self.action_table_model,
+                    'button': self.action_table_button,
+                    'success_message': self.action_table_success_message,
+                }
+            ]
+        elif self.action_table_multitables:
+            for action_table in self.action_table_multitables:
+                if not action_table.get('model'):
+                    raise ImproperlyConfigured(
+                        "You must specify model in action_table_multitables"
+                        " ({class_name}) to configure {mixin_name}".format(
+                            class_name=self.__class__,
+                            mixin_name=__class__.__name__,
+                        )
+                    )
+        else:
             raise ImproperlyConfigured(
                 "You must specify action_table_model or action_table_multitables"
-                " in {class_name} for configure {mixin_name}".format(
+                " ({class_name}) to configure {mixin_name}".format(
                     class_name=self.__class__,
                     mixin_name=__class__.__name__,
                 )
             )
 
-        for button_name, model in actions.items():
-            button = request.POST.get(button_name)
+        for action_table in self.action_table_multitables:
+            model = action_table['model']
+            button = action_table['button']
+            success_message = action_table['success_message']
+
+            button = request.POST.get(button)
             pks = request.POST.getlist(button)
             if pks:
                 selected_objects = model.objects.filter(pk__in=pks)
                 selected_objects.delete()
-                if self.success_delete_message:
-                    messages.success(self.request, self.success_delete_message)
+                if success_message:
+                    messages.success(self.request, success_message)
 
         return HttpResponseRedirect(request.path)
 
