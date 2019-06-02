ffd6335d9c04c8d70c6ffff3bd6b7a07b4b65f2b
crowmurk <crowmurk@gmail.com>
2019-06-02 09:53:21 +0300

    fix: No meassages show in delete views

--- locker/core/views.py
+++ locker/core/views.py
@@ -55,9 +55,9 @@ class ActionTableDeleteMixin:
 
 
 class SuccessDeleteMessageMixin:
-    success_delete_message = None
+    success_message = None
 
     def delete(self, request, *args, **kwargs):
-        if self.success_delete_message:
+        if self.success_message:
             messages.success(self.request, self.success_message)
         return super().delete(request, *args, **kwargs)
