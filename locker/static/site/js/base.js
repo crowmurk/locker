44632da1756f022fb8b7d5cbe0a7a3b5d67d843b
crowmurk <crowmurk@gmail.com>
2019-06-02 09:38:57 +0300

    feat: Show loading spinner while page prepared

--- locker/static/site/js/base.js
+++ locker/static/site/js/base.js
@@ -1,5 +1,10 @@
+$(window).on('load', function() {
+    $('.loading-progress').remove();
+    $('.body-container').css('visibility', 'visible');
+});
+
 $(document).ready(function() {
     $("ul.messages").delay(3000).fadeOut(1000, function() {
         $(this).remove();
     });
-})
+});
