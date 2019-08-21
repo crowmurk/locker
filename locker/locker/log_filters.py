90bee3e7af93ec456a95330e2818d5031ea392aa
crowmurk <crowmurk@gmail.com>
2019-08-21 05:53:18 +0300

    feat: Add Werkzeug debugger

--- locker/locker/log_filters.py
+++ locker/locker/log_filters.py
@@ -1,10 +1,13 @@
 from logging import Filter
 
-
 class ManagementFilter(Filter):
     def filter(self, record):
-        """Фильтрует вывод запросов БД из лога
+        """Фильтрует вывод лога
         """
-        if hasattr(record, 'funcName') and record.funcName == 'execute':
+        if hasattr(record, 'funcName') and record.funcName in [
+                'execute',
+                'tick',
+                'watch_dir',
+        ]:
             return False
         return True
