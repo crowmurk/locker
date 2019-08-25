a16aa8eb9c394923ed5dd81a07eec016e973a450
crowmurk <crowmurk@gmail.com>
2019-08-25 11:28:47 +0300

    fix: OrderOption form appearance

--- locker/static/site/js/calc-orderoption_form.js
+++ locker/static/site/js/calc-orderoption_form.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     var current_language = $("select[name='language']").val()
 
     // Add search option to select fields
-    $("#id_order").select2({language: current_language});
-    $("#id_service").select2({language: current_language});
+    $("#id_order").select2({language: current_language, width: '100%'});
+    $("#id_service").select2({language: current_language, width: '100%'});
 });
